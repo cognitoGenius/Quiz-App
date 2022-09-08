f488f8515f7648f3bfb09f9b9caa3203a37f40bc
Tommy, Ubongabasi <tommyubongabasijohn@gmail.com>
2022-09-08 22:04:22 +0100

    cookie options

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,7 +21,7 @@ const refactorResponse = (user, statusCode, res) => {
 
     const cookieOptions = {
         expires: new Date(
-            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+            Date.now() + 60 * 24 * 60 * 60 * 1000
         ),
         httpOnly: true
     };
